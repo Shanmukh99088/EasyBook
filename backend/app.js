e8cf28f2f388b8b72ddbee33ed1b8b6ca5d01697
Sai Kishore Reddy <84308565+saikishorereddy533@users.noreply.github.com>
2023-08-31 17:49:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://main.d1d0bnvalj6fok.amplifyapp.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify allowed HTTP methods
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Specify allowed headers
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
